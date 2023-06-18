20d73a900607193e3c8e2d5d150dda05a8f6b52b
IvanStoilovski <ivanstoilovski1@gmail.com>
2023-06-18 18:06:48 +0200

    Created a method for adding movies

--- backend/src/main/java/mk/ukim/finki/mea_pellicula/api/MovieController.java
+++ backend/src/main/java/mk/ukim/finki/mea_pellicula/api/MovieController.java
@@ -1,13 +1,12 @@
 package mk.ukim.finki.mea_pellicula.api;
 
+import mk.ukim.finki.mea_pellicula.model.Movie;
 import mk.ukim.finki.mea_pellicula.model.UpcomingProjectionsView;
 import mk.ukim.finki.mea_pellicula.service.MovieService;
 import mk.ukim.finki.mea_pellicula.model.MovieProjectionsView;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,16 @@ public class MovieController {
     @GetMapping("/upcoming-projection/{movieId}")
     List<UpcomingProjectionsView> getInfoForUpcomingProjection(@PathVariable Long movieId) {
         return movieService.getInfoAboutUpcomingProjectionsOfMovie(movieId);
+    }
+    @PostMapping("/add-movie/{movieId}")
+    Movie addMovie(@PathVariable(name = "movieId") Long id,
+                   @RequestParam(name="title", required = true) String title,
+                   @RequestParam(name="description", required = true) String description,
+                   @RequestParam(name="dateReleased", required = true) LocalDateTime dateTime,
+                   @RequestParam(name="imdbLink", required = true) String imdbLink,
+                   @RequestParam(name="duration", required = true) Integer duration,
+                   @RequestParam(name="is3d", required = true) Boolean is3d)
+    {
+      return movieService.saveOrUpdateMovie(id, title,description,dateTime,imdbLink,duration,is3d);
     }
 }
--- backend/src/main/java/mk/ukim/finki/mea_pellicula/service/MovieService.java
+++ backend/src/main/java/mk/ukim/finki/mea_pellicula/service/MovieService.java
@@ -1,22 +1,28 @@
 package mk.ukim.finki.mea_pellicula.service;
 
+import mk.ukim.finki.mea_pellicula.model.Movie;
 import mk.ukim.finki.mea_pellicula.model.MovieProjectionsView;
 import mk.ukim.finki.mea_pellicula.model.UpcomingProjectionsView;
 import mk.ukim.finki.mea_pellicula.repo.MovieProjectionsRepository;
+import mk.ukim.finki.mea_pellicula.repo.MovieRepository;
 import mk.ukim.finki.mea_pellicula.repo.UpcomingProjectionsRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class MovieService {
     private final MovieProjectionsRepository movieProjectionsRepository;
     private final UpcomingProjectionsRepository upcomingProjectionsRepository;
+    private final MovieRepository movieRepository;
 
     public MovieService(MovieProjectionsRepository movieProjectionsRepository,
-                        UpcomingProjectionsRepository upcomingProjectionsRepository) {
+                        UpcomingProjectionsRepository upcomingProjectionsRepository,
+                        MovieRepository movieRepository) {
         this.movieProjectionsRepository = movieProjectionsRepository;
         this.upcomingProjectionsRepository = upcomingProjectionsRepository;
+        this.movieRepository=movieRepository;
     }
 
     public List<MovieProjectionsView> getAllMovieProjectionsForNextMonth() {
@@ -25,5 +31,23 @@ public class MovieService {
 
     public List<UpcomingProjectionsView> getInfoAboutUpcomingProjectionsOfMovie(Long movieId) {
         return upcomingProjectionsRepository.findAllByMovieId(movieId);
+    }
+    public Movie saveOrUpdateMovie(Long id, String title, String description, LocalDateTime dateReleased, String imdbLink, Integer duration, Boolean is3d )
+    {
+        Movie m=movieRepository.findMovieById(id);
+        if(m!=null)
+        {
+            m.setTitle(title);
+            m.setDescription(description);
+            m.setDateReleased(dateReleased);
+            m.setImdbLink(imdbLink);
+            m.setDuration(duration);
+            m.setIs3d(is3d);
+        }
+        else
+        {
+         m=new Movie(title,description,dateReleased,imdbLink,duration,is3d);
+        }
+        return movieRepository.save(m);
     }
 }
